1e0560775e31107b1f6cac660836c44fdfe7ec5d
karanlaxmanshanbhag <shanbhagkaran@gmail.com>
2024-10-24 18:27:15 +0530

    Add files via upload

---
+++ JOtionPane.java
@@ -0,0 +1,19 @@
+
+import javax.swing.ImageIcon;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Admin
+ */
+class JOtionPane {
+
+    static void showMessageDialog(Object object, String htmlb_stylecolor_red_fontsize_10pxIncorre, String show, int INFORMATION_MESSAGE, ImageIcon icon) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
